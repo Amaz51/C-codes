edef55125369f2183e9b361967032d91df278894
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-10 11:00:16 +0500

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+	    cout << "                                                                    "<<endl;
+		cout << "                                                                    "<<endl;
+		cout << "                                                                    "<<endl;
+		cout << "                                                                    "<<endl;
+		cout << "                                                                    "<<endl;
+		cout << "                                        LL         UU    UU     DDDDDDD     OOOOOOO       "<<endl;
+        cout << "                                        LL         UU    UU     DD   DD     OO   OO       "<<endl;
+        cout << "                                        LL         UU    UU     DD   DD     OO   OO       "<<endl;
+        cout << "                                        LL         UU    UU     DD   DD     OO   OO       "<<endl;
+        cout << "                                        LLLLLL     UUUUUUUU     DDDDDDD     OOOOOOO       "<<endl;
+        cout << "                                                                    "<<endl;
+        cout << "                                                                    "<<endl;
+        cout << "                                                                    "<<endl;
+
+}
