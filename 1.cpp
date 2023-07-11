969ef0f3a533e355ce49c1fb86ecb09b41138c45
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-11 10:08:54 +0500

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,63 +1,40 @@
 //#include<iostream>
-//#include<fstream>
 //using namespace std;
-//int** InputMatrix(ifstream& ffin, int& rows, int& cols) {
-//    ffin >> rows >> cols; // Read the matrix size from file
-//
-//    // Create the matrix dynamically
-//    int** matrixA = new int* [rows];
-//    int** matrixB = new int* [rows];
-//    int** matrixC = new int* [rows];
-//    for (int i = 0; i < rows; i++) {
-//        matrixA[i] = new int[cols];
-//        matrixB[i] = new int[cols];
-//        matrixC[i] = new int[cols];
-//    }
-//
-//    // Read the matrix elements from file
-//    for (int i = 0; i < rows; i++) {
-//        for (int j = 0; j < cols; j++) {
-//            ffin >> matrixA[i][j];
-//        }
-//    }
-//    for (int i = 0; i < rows; i++) {
-//        for (int j = 0; j < cols; j++) {
-//            ffin >> matrixB[i][j];
-//        }
-//    }
-//    for (int i = 0; i < rows; i++) {
-//        for (int j = 0; j < cols; j++) {
-//            ffin >> matrixC[i][j];
-//        }
-//    }
-//    ffin.close();
-//    return matrixA; // Return the created matrix
+//void swap(int arr[],int s) {
+//	for (int i = 0; i < s; i++) {
+//		int temp = 0;
+//		for (int j = i + 1; j < s; j++) {
+//			if (arr[j] < arr[i]) {
+//				temp = arr[j];
+//				arr[j] = arr[i];
+//				arr[i] = temp;
+//			}
+//		}
+//	}
 //}
-//void OutputMatrix(int** matrix, const int& ROWS, const int& COLS) {
-//    for (int i = 0; i < ROWS; i++) {
-//        for (int j = 0; j < COLS; j++) {
-//            cout << matrix[i][j]<< " ";
-//        }cout << endl;
-//    }
+//void printarr(int arr[],int size) {
+//	for (int i = 0; i < size; i++) {
+//		cout << *(arr + i) << " ";
+//
+//	}
 //}
 //int main() {
+//	int size;
+//	cout << "Enter the size of array : ";
+//	cin >> size;
 //
-//    ifstream file;
-//    file.open("file.txt");
-//    
-//    int rows, cols;
-//    
-//    int** matrixA = InputMatrix(file, rows, cols);
-//    int** matrixB = InputMatrix(file, rows, cols);
-//
-//    // Use the matrix here
-//    OutputMatrix(matrixA, rows, cols);
-//    OutputMatrix(matrixB, rows, cols);
-//    // Free the memory allocated for the matrix
-//    for (int i = 0; i < rows; i++) {
-//        delete[] matrixA[i];
-//    }
-//    delete[] matrixA;
-//
-//    return 0;
+//	int *arr = new int[size];
+//	cout << "Enter the array elements : ";
+//	for (int i = 0; i < size; ) {
+//		cin >> *(arr+i);
+//		if (arr[i] == 0 || arr[i] == 1) {
+//			i++;
+//		}
+//		else {
+//			cout << "Please enter 0 or 1.";
+//		}
+//	}
+//	swap(arr, size);
+//	printarr(arr,size);
+// delete[] arr;
 //}
