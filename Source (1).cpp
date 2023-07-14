3ac50b514ec0d5343f6a3bdce54e72128ff194d7
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-14 09:43:46 +0500

    Add files via upload

---
+++ Source (1).cpp
@@ -0,0 +1,42 @@
+ #include<iostream>
+ using namespace std;
+ int mian(){
+	int i,rev=0;
+	cin>>i;
+	for(int num=0;i>0){
+    r=i%10;
+	i=(rev*10)+r;
+	
+	if(r>0){
+	if(r==0){
+		cout<<"zero";
+	}else if (r==1){
+		cout<<"one";
+	}
+	else if (r==2){
+		cout<<"two";
+	}
+	else if (r==3){
+		cout<<"Three";
+	}
+	else if (r==4){
+		cout<<"Four";
+	}
+	else if (r==5){
+		cout<<"Five";
+	}
+	else if (r==6){
+		cout<<"six";
+	}
+	else if (r==7){
+		cout<<"seven";
+	}
+	else if (r==8){
+		cout<<"eight";
+	}
+	else if (r==9){
+		cout<<"nine";
+	}
+	}
+	}
+ }
