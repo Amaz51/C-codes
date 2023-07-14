3765b1e9eecd5f782974beb80a5cff6ce184b883
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-14 09:39:46 +0500

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,36 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Enter the number of rows : ";
-    cin>>n;
-  //inverted pyramid//
-    for(int i=1;i<=n;i++){
-        for(int j=0;j<=i-1;j++){
-            cout<<" ";
-        }
-        for(int k=1;k<=n-i+1;k++){
-            cout<<"* ";
-        }cout<<endl;
+    for(int i=1;i<=3;i++){
+
+          for(intj=1;j<=3;j++){
+            cout<<i<<endl;
+          }       
     }
-
-    //right angle triangle//
-
-    for(int i=1;i<=n;i++){
-         for(int k=1;k<i;k++){
-            cout<<"* ";
-        }
-       cout<<endl;
-    }
-
-    //inverted right angle triangle//
-
-     for(int i=1;i<=n;i++){
-         for(int k=1;k<=n-i+1;k++){
-            cout<<"* ";
-        }
-       cout<<endl;
-    }
-
-   
 }
