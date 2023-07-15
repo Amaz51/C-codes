2762f9a6c57f6fcc8b2edf179e4404c903417f56
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-15 11:09:56 +0500

    Add files via upload

--- Q4.cpp
+++ Q4.cpp
@@ -1,16 +1,49 @@
 #include<iostream>
 using namespace std;
-int main(){
-    
-    for(int i=1;i<=4;i++){
-        int spc=4-i;
-        for(int j=1;j<=spc;j++){
-            cout<<" ";
-        }
-        for(int k=1;k<=2*i-1;k++){
-            cout<<"*";
-        }
-        spc--;
-        cout<<endl;
-    }
+int main() {
+
+
+	int a = 1;
+	while (a <= 5) {
+		int b = 1;
+		while (b <= 5 - a) {
+			cout << " ";
+			b++;
+		}
+		int c = 1;
+
+		while (c <= a) {
+			while (c != 1 && c != a) {
+				cout << "  ";
+				c++;
+				
+		   }
+			cout << "# ";
+			c++;
+		}
+		cout << endl;
+
+		a++;
+	}
+	int d = 5;
+	i = 1;
+	while (i <= 5) {
+		int b = 1;
+		while (b <= a) {
+			cout << " ";
+			b++;
+		}
+		int c = d;
+		while (c > 1) {
+			while (c != 2 && c != d) {
+				cout << "  ";
+				c--;
+			}		
+			cout << "# ";
+			c--;
+		} d--;
+		cout << endl;
+
+		a++;
+	}
 }
