0ddf00d5f440e02e3e38931a13be6d46d78e18f8
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-15 11:09:20 +0500

    Add files via upload

--- Q3.cpp
+++ Q3.cpp
@@ -1,19 +1,40 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int i=1;
-    int j=1;
-    do{
-       j=1;
-       do{ 
-           cout<<i<<" "<<j<<endl;
-           j++;
-       }
-          
-    while(j<=3);
-     i++;
-     }
-    while(i<=3);
-    
-    
+int main() {
+
+	
+	int a = 1;
+	while (a <= 5) {
+		int b = 1;
+		while (b <= 5- a) {
+			cout << " ";
+			b++;
+		}
+		int c = 1;
+
+		while (c <= a) {
+			cout << "* ";
+			c++;
+		}
+		cout << endl;
+
+		a++;
+	}
+	int s = 5;
+	a = 1;
+	while (a <= 5) {
+		int b = 1;
+		while (b <= a) {
+			cout << " ";
+			b++;
+		}
+		int c = s;
+		while (c > 1) {
+			cout << "* ";
+			c--;
+		} s--;
+		cout << endl;
+
+		a++;
+	}
 }
