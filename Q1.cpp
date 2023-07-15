b5bea937ea75bade4b4e53e866a6b2d513c0b3a4
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-15 11:08:06 +0500

    Add files via upload

--- Q1.cpp
+++ Q1.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int n,r,f;
-    cout<<"Enter any number : ";
-    cin>>n;
-    
-    for(int i=0;i<10;i++){
-        f=0;
-        cout<<"The frequency of the number "<<i<<"is :";
-        for(int j=n;j>0;j=j/10){
-        r=j%10;
-        if(r==i){
-           f++;
+int main()
+{
+    int i = 0, rows;
+    cout << "Enter the number of rows : ";
+    cin >> rows;
+    while (i < rows) {
+        int u = rows;
+        while (u > i) {
+            cout << "*";
+            u--;
         }
-        }cout<<f<<endl;
+        cout << endl;
+        i++;
+    }i++;
+    while (i < (rows + rows)) {
+        int f = 0;
+        while (f <= (i - rows)) {
+            cout << "*";
+            f++;
+        }i++;
+        cout << endl;
     }
 }
