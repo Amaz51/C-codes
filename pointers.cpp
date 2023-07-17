38f67c3ab20fd512b4f0fb8ab0797c5026b772e3
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2023-07-17 08:20:30 +0500

    Add files via upload

---
+++ pointers.cpp
@@ -0,0 +1,42 @@
+//#include<iostream>
+//using namespace std;
+////void swap(int* a, int* b) {
+////	int temp = *a;
+////	*a = *b;
+////	*b = temp;
+////}
+////int main() {
+////	/*int a = 10;
+////	int *aptr = &a;
+////	cout << *aptr << endl;
+////	*aptr = 20;
+////	cout << a << endl;
+////	cout << &a << endl;*/
+////	///*int arr[] = { 10,20,30 };
+////	//cout << arr[0] << endl;
+////	//int *ptr = arr;
+////	//for (int i = 0; i < 3; i++) {
+////	//	cout << *ptr << endl;
+////	//	ptr++;
+////	//}*/
+////	/////*int a = 2;
+////	////int b = 4;
+////	////swap(&a, &b);
+////	////cout << a << " " << b << endl;*/
+////	
+////}
+//class Car {
+//public:
+//	int speed(int maxSpeed);
+//
+//};
+//
+//int Car::speed(int maxSpeed) {
+//	return maxSpeed;
+//}
+//
+//int main() {
+//	Car myObj; // Create an object of Car
+//	cout << myObj.speed(200); // Call the method with an argument
+//	return 0;
+//}
